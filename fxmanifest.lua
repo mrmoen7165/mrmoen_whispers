ce35ba7d32a7c39974d990adc080a518281b05fa
Eirik M Moen <eirikmoen333@hotmail.com>
2025-11-06 11:41:38 +0100

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 author 'MrMoen'
 description 'mrmoen_whispers - Dynamic graveyard & forest hauntings for RedM (RSGCore)'
-version '1.0.0'
+version '1.0.5'
 
 shared_script '@ox_lib/init.lua'
 
